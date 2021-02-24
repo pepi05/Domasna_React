84e553ab101bcacc7d5a886046ee3e7bd6cd6ad3
Petar <72278293+pepi05@users.noreply.github.com>
2021-02-24 22:24:15 +0100

    'togglefunctionupdated'

--- homework04/homeapp4/src/App.js
+++ homework04/homeapp4/src/App.js
@@ -8,14 +8,24 @@
   
     3. For the given photo, implement function that will handle how many times the image has been clicked. Show the result. 
   */
-import { useState } from 'react';
+ import { useState, useEffect } from 'react';
 
 const App = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [age, setAge] = useState('');
+  const [age, setAge] = useState(0);
   const [adress, setAdress] = useState('');
   const [gender, setGender] = useState('');
+  const[showData, setShowData] = useState(true)
+
+  // useEffect hook
+
+  // Task 02 - change the value for the age data for the user. If the age is greather than 18 show the age, else show message
+  // that the use must be +18 to submit the form
+
+  useEffect(() => {
+  return  (age >= 18) ? console.log(age) : console.warn('user must be 18+')
+  }, [age])
 
 
 
@@ -26,7 +36,7 @@ const onInputLastName = (event) => {
   setLastName(event.target.value);
  }
  const onInputAge = (event) => {
-   setAge(event.target.value)
+   setAge(parseInt(event.target.value))
  }
  const onInputAdress = (event) => {
    setAdress(event.target.value)
@@ -35,15 +45,22 @@ const onInputLastName = (event) => {
    setGender(event.target.value)
  }
 
+ const toggleData = () => {
+   setShowData(state => {
+     return !state;
+   })
+ }
 
- const toggle = () => {
-  let x = document.getElementById('info');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-  } else {
-    x.style.display = 'none';
-  }
-}
+
+//  const toggle = () => {
+//   let x = document.getElementById('info');
+//   if (x.style.display === 'none') {
+//     x.style.display = 'block';
+//   } else {
+//     x.style.display = 'none';
+//   }
+// }
+
 
 
 const [clicker, setClicker] = useState(0)
@@ -57,15 +74,18 @@ const onImgClick = () => {
   return (
    <div id='app'>
      
-    First name: <input type='text' placeholder='first name' value={firstName} onChange={onInputFirstName}></input> <br />
-    Last name:  <input type='text' placeholder='last name' value={lastName} onChange={onInputLastName}></input> <br />
-    age: <input type='number' placeholder='age' value={age} onChange={onInputAge}></input> <br />
-    Adress: <input type='text' placeholder='adress' value={adress} onChange={onInputAdress}></input> <br />
-    Gender: <input type='text' placeholder='gender' value={gender} onChange={onInputGender}></input> <br />
+    First name: <input type='text' name='firstName' placeholder='first name' value={firstName} onChange={onInputFirstName}></input> <br />
+    Last name:  <input type='text' name='lastName' placeholder='last name' value={lastName} onChange={onInputLastName}></input> <br />
+    age:        <input type='number' name='age' placeholder='age' value={age} onChange={onInputAge}></input> <br />
+    Adress:     <input type='text' name='adress' placeholder='adress' value={adress} onChange={onInputAdress}></input> <br />
+    Gender:     <input type='text' name='gender' placeholder='gender' value={gender} onChange={onInputGender}></input> <br />
    
-    <button onClick={toggle}>show/hide</button>
+    {/* <button onClick={toggle}>show/hide</button> */}
+    <button onClick={toggleData}>Click me</button>
 
-    <div id='info'>
+    {showData &&
+
+    <div id='user data'>
       <p>First name: {firstName}</p>
       <p>Last name: {lastName} </p>
       <p>age: {age}</p>
@@ -73,7 +93,7 @@ const onImgClick = () => {
       <p>Gender: {gender}</p>
 
     </div>
-    
+}
     
     <div id='image'>
 
